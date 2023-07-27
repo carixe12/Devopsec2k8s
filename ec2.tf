f439dd7d4231d12c395b030d92e3f56fdace3c8f
carixe12 <claudiocarixe@gmail.com>
2023-07-27 11:11:34 +0300

    k8s

---
+++ ec2.tf
@@ -0,0 +1,30 @@
+resource "aws" "myec2" {
+  ami                    = "ami-053b0d53c279acc90"
+  instance_type          = "t2.medium"
+  availibility_zonw      = "us-west-2"
+  vpc_security_group_ids = [aws_security_group.allow_tls.id]
+  key_name = "mykey"
+
+
+  provisioner "remote-exec" {
+    inline = [
+      "sudo apt update -y",
+      "sudo apt install docker.io -y",
+      "sudo wget https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64",
+      "sudo chmod +x /home/ubuntu/minikube-linux-amd64",
+      "sudo cp minikube-linux-amd64 /usr/local/bin/minikube",
+      "curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl",
+      "sudo chmod +x /home/ubuntu/kubectl",
+      "sudo cp kubectl /usr/local/bin/kubectl",
+      "sudo usermod -aG docker ubuntu"
+    ]
+
+    connection {
+      type        = "ssh"
+      host        = self.public_ip
+      user        = "ubuntu"
+      private_key = file("./mykey.pem")
+
+    }
+  }
+}
